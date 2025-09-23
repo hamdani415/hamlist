5a08661237044f0553e8555341c51e1c95fdd466
hamdani415 <neswakun27@gmail.com>
2025-09-24 05:53:41 +0700

    update hamlist 4.0

--- src/components/pengeluaran/pengeluaranComponents/tambahdata.jsx
+++ src/components/pengeluaran/pengeluaranComponents/tambahdata.jsx
@@ -2,22 +2,23 @@
 import { CheckFatIcon } from '@phosphor-icons/react'
 import React, { useState } from 'react'
 
-const Tambahfinancial = ({ email, user,getData }) => {
+const Tambahfinancial = ({ email, user, getData }) => {
     const [pengeluaran, setpengeluaran] = useState('')
     const [harga, setharga] = useState('')
     const [tanggal, settanggal] = useState('')
-    const [message , setMessage] = useState('')
+    const [message, setMessage] = useState('')
+    const [kategory, setkategory] = useState('')
     // const tanggal = new Date().toISOString().slice(0, 10)
     const [loading, setloading] = useState(false)
     const [open, setopen] = useState(false)
 
 
-   
+
 
     const handletambahdata = async (e) => {
         e.preventDefault()
         setloading(true)
-        const data = { tanggal : new Date(tanggal), pengeluaran, harga, email }
+        const data = { tanggal: new Date(tanggal), pengeluaran, harga, email , kategory_id : kategory }
         const response = await fetch(`${process.env.NEXT_PUBLIC_API}/financial`, {
             method: 'POST',
             headers: {
@@ -33,9 +34,9 @@ const Tambahfinancial = ({ email, user,getData }) => {
             setharga('')
             setopen(false)
             getData()
-        } else if(hasil.errors && hasil.errors.length > 0) {
-           setMessage(hasil.errors?.[0]?.msg)
-        } else{
+        } else if (hasil.errors && hasil.errors.length > 0) {
+            setMessage(hasil.errors?.[0]?.msg)
+        } else {
             setMessage("terjadi kesalahan")
         }
         setloading(false)
@@ -50,6 +51,10 @@ const Tambahfinancial = ({ email, user,getData }) => {
     const handleharga = (e) => {
         const value = parseInt(e.target.value)
         setharga(value)
+    }
+    const handleKategory = (e) => {
+        const value = parseInt(e.target.value)
+        setkategory(value)
     }
 
     const handletombolopen = (e) => {
@@ -74,7 +79,7 @@ const Tambahfinancial = ({ email, user,getData }) => {
             <button onClick={handletombolopen} className='text-4xl font-bold text-green-500'>+</button>
             {open && (
                 <div className=' fixed right-0.5 w-full flex justify-center items-center'>
-                    <div className='bg-green-300  rounded-xl md:w-2/4 sm:w-3/4 w-full mx-4 pb-8 mt-10 z-10 shadow-black shadow-xl'>
+                    <div className='bg-green-300  rounded-xl md:w-2/4 sm:w-3/4 w-full mx-4 pb-8 mb-10 z-10 shadow-black shadow-xl'>
                         <h1 className='p-2 font-bold text-blue-500'>EXPENSES</h1>
                         <div className='p-4 '>
                             <div className=''>
@@ -84,6 +89,22 @@ const Tambahfinancial = ({ email, user,getData }) => {
                             <div className='py-4'>
                                 <p className='text-white font-semibold'>PENGELUARAN</p>
                                 <input type="text" className='w-full md:px-8 sm:px-8 text-blue-500 font-semibold p-2 border border-white rounded-2xl bg-white' placeholder='makan..' value={pengeluaran} onChange={handlepengeluaran} />
+                            </div>
+                            <div className='pb-4'>
+                                <p className='text-white font-semibold'>KATEGORI</p>
+                                <select onChange={handleKategory} className='w-full md:px-8 sm:px-8 text-blue-500 font-semibold p-2 border border-white rounded-2xl bg-white' name="" id="">
+                                    <option value="">-Pilih kategori-</option>
+                                    <option value="1">Makanan & Minuman pokok</option>
+                                    <option value="2">Makanan & Minuman jajan</option>
+                                    <option value="3">Transportasi</option>
+                                    <option value="4">Tagihan</option>
+                                    <option value="5">Kesehatan</option>
+                                    <option value="6">Pendidikan</option>
+                                    <option value="7">Belanja</option>
+                                    <option value="8">Tabungan/Investasi</option>
+                                    <option value="9">Hiburan</option>
+                                    <option value="10">Lain-lain</option>
+                                </select>
                             </div>
                             <div className=''>
                                 <p className='text-white font-semibold'>TOTAL</p>
